6b9d4b8e0dfd575e96b45f1fa22d6e3dcee3950d
Mateus Rabelo Horta <mts.horta@gmail.com>
2024-06-25 16:37:46 -0300

    Adicionando
    Adicionando os arquivos do pc para o github

---
+++ app.py
@@ -0,0 +1,37 @@
+#Bibliotecas necessárias
+import pandas as pd
+import numpy as np
+import datetime
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import streamlit as st
+
+
+st.set_page_config(page_title='Financiamento 360')
+
+#
+#
+#
+#Seleção do prazo do financiamento
+range = range(0, 421)
+periodo = st.select_slider(
+    "Selecione o período do seu financiamento",
+    options=range, value =420)
+st.write(f'O prazo selecionado corresponde à {periodo/12} anos')
+
+#
+#
+#
+#Seleção do juro anual
+taxa_anual = st.number_input("Insira o valor dos juros efetivos (anual): ")
+taxa_mensal = (1+taxa_anual/100)**(1/12)-1
+st.write("Com ", taxa_anual,"%","a.a de juros efetivos, temos uma taxa equivalente de juro mensal de: ", taxa_mensal )
+
+st.text('A fórmula para o cálculo da parcela é:')
+st.latex(r'''\text{PMT} = \frac{P \cdot r \cdot (1 + r)^n}{(1 + r)^n - 1}
+''')
+
+st.text('''Em que:
+        P = valor financiado.
+        r = taxa de juros.
+        n = prazo.''')
